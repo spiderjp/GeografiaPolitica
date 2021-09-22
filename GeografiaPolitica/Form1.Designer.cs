c0a521dfe67f1597e2e341edba561fc0c3f24318
spiderjp <jotapejpbsf@gmail.com>
2021-09-22 11:34:52 -0300

    Programa Finalizado

---
+++ GeografiaPolitica/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace GeografiaPolitica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtTexto = new System.Windows.Forms.TextBox();
+            this.gbPais = new System.Windows.Forms.GroupBox();
+            this.argentina = new System.Windows.Forms.RadioButton();
+            this.mexico = new System.Windows.Forms.RadioButton();
+            this.chile = new System.Windows.Forms.RadioButton();
+            this.brasil = new System.Windows.Forms.RadioButton();
+            this.pbImagem = new System.Windows.Forms.PictureBox();
+            this.gbPais.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtTexto
+            // 
+            this.txtTexto.Location = new System.Drawing.Point(155, 19);
+            this.txtTexto.Multiline = true;
+            this.txtTexto.Name = "txtTexto";
+            this.txtTexto.Size = new System.Drawing.Size(328, 122);
+            this.txtTexto.TabIndex = 0;
+            this.txtTexto.Text = resources.GetString("txtTexto.Text");
+            // 
+            // gbPais
+            // 
+            this.gbPais.Controls.Add(this.argentina);
+            this.gbPais.Controls.Add(this.mexico);
+            this.gbPais.Controls.Add(this.chile);
+            this.gbPais.Controls.Add(this.brasil);
+            this.gbPais.Location = new System.Drawing.Point(43, 12);
+            this.gbPais.Name = "gbPais";
+            this.gbPais.Size = new System.Drawing.Size(106, 129);
+            this.gbPais.TabIndex = 1;
+            this.gbPais.TabStop = false;
+            this.gbPais.Text = "Selecione o Pais";
+            this.gbPais.Enter += new System.EventHandler(this.gbPais_Enter);
+            // 
+            // argentina
+            // 
+            this.argentina.AutoSize = true;
+            this.argentina.Location = new System.Drawing.Point(7, 89);
+            this.argentina.Name = "argentina";
+            this.argentina.Size = new System.Drawing.Size(70, 17);
+            this.argentina.TabIndex = 0;
+            this.argentina.TabStop = true;
+            this.argentina.Text = "Argentina";
+            this.argentina.UseVisualStyleBackColor = true;
+            this.argentina.CheckedChanged += new System.EventHandler(this.argentina_CheckedChanged);
+            // 
+            // mexico
+            // 
+            this.mexico.AutoSize = true;
+            this.mexico.Location = new System.Drawing.Point(7, 66);
+            this.mexico.Name = "mexico";
+            this.mexico.Size = new System.Drawing.Size(59, 17);
+            this.mexico.TabIndex = 0;
+            this.mexico.TabStop = true;
+            this.mexico.Text = "México";
+            this.mexico.UseVisualStyleBackColor = true;
+            this.mexico.CheckedChanged += new System.EventHandler(this.mexico_CheckedChanged);
+            // 
+            // chile
+            // 
+            this.chile.AutoSize = true;
+            this.chile.Location = new System.Drawing.Point(7, 43);
+            this.chile.Name = "chile";
+            this.chile.Size = new System.Drawing.Size(48, 17);
+            this.chile.TabIndex = 0;
+            this.chile.TabStop = true;
+            this.chile.Text = "Chile";
+            this.chile.UseVisualStyleBackColor = true;
+            this.chile.CheckedChanged += new System.EventHandler(this.chile_CheckedChanged);
+            // 
+            // brasil
+            // 
+            this.brasil.AutoSize = true;
+            this.brasil.Checked = true;
+            this.brasil.Location = new System.Drawing.Point(7, 20);
+            this.brasil.Name = "brasil";
+            this.brasil.Size = new System.Drawing.Size(50, 17);
+            this.brasil.TabIndex = 0;
+            this.brasil.TabStop = true;
+            this.brasil.Text = "Brasil";
+            this.brasil.UseVisualStyleBackColor = true;
+            this.brasil.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // pbImagem
+            // 
+            this.pbImagem.Image = global::GeografiaPolitica.Properties.Resources.brasil;
+            this.pbImagem.Location = new System.Drawing.Point(43, 147);
+            this.pbImagem.Name = "pbImagem";
+            this.pbImagem.Size = new System.Drawing.Size(440, 291);
+            this.pbImagem.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbImagem.TabIndex = 2;
+            this.pbImagem.TabStop = false;
+            this.pbImagem.Click += new System.EventHandler(this.pbImagem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 450);
+            this.Controls.Add(this.pbImagem);
+            this.Controls.Add(this.gbPais);
+            this.Controls.Add(this.txtTexto);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Geografia Política - João Pedro Ferreira de Souza Batista - RGM: 2316257-1";
+            this.gbPais.ResumeLayout(false);
+            this.gbPais.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTexto;
+        private System.Windows.Forms.GroupBox gbPais;
+        private System.Windows.Forms.RadioButton argentina;
+        private System.Windows.Forms.RadioButton mexico;
+        private System.Windows.Forms.RadioButton chile;
+        private System.Windows.Forms.RadioButton brasil;
+        private System.Windows.Forms.PictureBox pbImagem;
+    }
+}
+
---
+++ GeografiaPolitica/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeografiaPolitica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        String image = "";
+        String text = "";
+
+        public void alterContries(String contry, String txt) {
+
+            image = contry + ".png";
+
+            if (brasil.Checked == true)
+            {
+                contry = "brasil";
+                text = "O país é o quinto maior do mundo em área territorial, com 8 510 345,538 km² " +
+                    "O Brasil tem um sistema denso e complexo de rios, um dos mais extensos do mundo, com oito grandes bacias hidrográficas. " +
+                    "A organização político-administrativa da República Federativa do Brasil compreende a União, os estados," +
+                    "o Distrito Federal e os municípios, todos autônomos, nos termos da constituição. " +
+                    "Todos os membros do executivo e do legislativo são eleitos diretamente.";
+            }
+            else if (chile.Checked == true)
+            {
+                contry = "chile";
+                text = "O território chileno é cortado ao norte pelo Trópico de Capricórnio." +
+                    "Vários rios de curta extensão e de baixo fluxo cortam seu território," +
+                    " que comumente fluem da cordilheira dos Andes até o Oceano Pacífico, no sentido leste-oeste." +
+                    "A atual Constituição do Chile foi aprovada em um plebiscito nacional fortemente contestado em setembro de 1980." +
+                    "O poder executivo é chefiado pelo Presidente da República, que é o chefe de Estado e o chefe de governo.";
+            }
+            else if (mexico.Checked == true)
+            {
+                contry = "mexico";
+                text = "O México é um país situado entre os Estados Unidos e a América Central.o " +
+                    "No norte, o país divide uma fronteira de 3 141 quilômetros com os Estados Unidos. " +
+                    "O México é atravessado de norte a sul por duas cadeias de montanhas conhecidas como Sierra Madre Oriental e Sierra Madre Ocidental." +
+                    "Ruínas antigas, como Teotihuacán e a cidade maia de Chichén Itzá, estão espalhadas pelo país. " +
+                    "Os Estados Unidos Mexicanos são uma federação cujo governo é representativo, democrático e republicano. " +
+                    "Três partidos têm sido historicamente os partidos dominantes na política mexicana.";
+            }
+            else if (argentina.Checked == true)
+            {
+                contry = "argentina";
+                text = "A Argentina está situada no sul da América do Sul, com a Cordilheira dos Andes a oeste. " +
+                    "O ponto mais alto acima do nível do mar é o Monte Aconcágua, na província de Mendoza. " +
+                    "Plantas subtropicais dominam o Gran Chaco, no norte, com o gênero de árvores Dalbergia. " +
+                    "O governo nacional é composto por três ramos:" +
+                    "Poder Executivo, Judiciário e Legislativo.";
+            }
+
+            pbImagem.Load("../../Resources/" + image);
+            txtTexto.Text = text;
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            alterContries("brasil", text);
+        }
+
+        private void gbPais_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chile_CheckedChanged(object sender, EventArgs e)
+        {
+            alterContries("chile", text);
+        }
+
+        private void mexico_CheckedChanged(object sender, EventArgs e)
+        {
+            alterContries("mexico", text);
+        }
+
+        private void argentina_CheckedChanged(object sender, EventArgs e)
+        {
+            alterContries("argentina", text);
+        }
+
+        private void pbImagem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
